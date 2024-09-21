87c08b9d4712333ac9a088cb77adb5c173d04dba
melisa176 <carolinacastillon27@gmail.com>
2024-09-20 19:56:40 -0500

    Verificar 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,18 @@
 # Almacenar la contraseña en una variable
+
 password_guardada= "JuanNeira2024"
-password_ingresada = input("Favor introduce la contraseña: ")
-if password_ingresada.lower() == password_guardada.lower():
-    print("La contraseña es correcta.")
-else:
-    print("La contraseña es incorrecta.")
+intentos = 0
+max_intentos = 5
+while intentos < max_intentos:
+    password_ingresada = input("Favor introduce la contraseña: ")
+    
+    if password_ingresada.lower() == password_guardada.lower():
+        print("La contraseña es correcta.")
+        break
+    else:
+        intentos +=1
+        print("La contraseña es incorrecta.")
+    
+if intentos == max_intentos:
+    print("Has alcanzado el número máximo de intentos. Salida del programa.")
     
